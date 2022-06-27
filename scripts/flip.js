a72be6e908ae0baf909cee33f41ce1c2483c31de
Brandon Poletti <brandon.poletti@gmail.com>
2022-06-27 15:39:13 -0400

    added flip code

---
+++ scripts/flip.js
@@ -0,0 +1,17 @@
+const card = document.querySelector('.card_inner');
+
+card.addEventListener('click', function () {
+    card.classList.toggle('is-flipped');
+})
+
+const card_2 = document.querySelector('.card_inner_2');
+
+card_2.addEventListener('click', function () {
+    card_2.classList.toggle('is-flipped');
+})
+
+const card_3 = document.querySelector('.card_inner_3');
+
+card_3.addEventListener('click', function () {
+    card_3.classList.toggle('is-flipped');
+})
